0b7e70a30e281cd7bcade085cce0b91769e83123
uspehgp <z995959z>
2020-08-23 12:42:04 +0300

    Initial commit

---
+++ src/main/java/com/uspehgp/robotspring/RobotspringApplication.java
@@ -0,0 +1,13 @@
+package com.uspehgp.robotspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RobotspringApplication.class, args);
+    }
+
+}
